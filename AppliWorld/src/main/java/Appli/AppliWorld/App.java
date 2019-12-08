d0ed89bb6388fcb835bbc9b55eac18991a08bd90
SohamRoyNoel <soham.roy.developer@gmail.com>
2019-12-08 11:30:54 +0530

    CheckWindow-Testing

---
+++ AppliWorld/src/main/java/Appli/AppliWorld/App.java
@@ -0,0 +1,13 @@
+package Appli.AppliWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Appli_WORLD/src/AppliTest.java
@@ -0,0 +1,10 @@
+
+public class AppliTest {
+
+	public static void main(String[] args) {
+
+		Ey
+		
+	}
+
+}
